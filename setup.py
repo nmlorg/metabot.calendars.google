cf0e40e1e2e8325cdc3cbb657876f237b63a3e01
Daniel Reed <nmlorg@gmail.com>
2019-06-05 17:18:32 -0700

    Switch from pkg_resources-style to pkgutil-style namespace packages.
    I do not know exactly what this does. But it seems to be important to make pytest work in tox (fixes nmlorg/metabot#53).

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ import setuptools
 
 setuptools.setup(
     name='metabot.calendars.google',
-    version='0.0.2',
+    version='0.1',
     author='Daniel Reed',
     author_email='nmlorg@gmail.com',
     description='Google Calendar loader for metabot.',
     url='https://github.com/nmlorg/metabot.calendars.google',
     packages=setuptools.find_packages(include=('metabot', 'metabot.*')),
-    namespace_packages=setuptools.find_packages(include=('metabot', 'metabot.*')),
-    install_requires=['google-api-python-client', 'metabot >= 0.0.1', 'oauth2client'])
+    install_requires=[
+        'google-api-python-client',
+        'metabot >= 0.2',
+        'oauth2client',
+    ])
